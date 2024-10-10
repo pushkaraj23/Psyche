9688a32ee79b6fc89dfb39170be713c87a0b83ee
pushkaraj23 <pushkaraj232002@gmail.com>
2024-10-10 22:39:30 +0530

    first commit

---
+++ psyche/src/components/nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Nav
+//This will be handled by Mahati
---
+++ psyche/src/pages/services.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Services
+//This page will be handled by Shashank
