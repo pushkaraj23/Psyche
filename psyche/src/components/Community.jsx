f31bba5aa99d6ece197bd1bd923721e97f33a798
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-10-27 15:36:26 +0530

    feat: add community

---
+++ psyche/src/components/Community.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ContactSection from "../components/JoinContact";
+
+const ContactUs = () => {
+  const title = "Join the Psyche Community";
+  const description =
+    "Stay informed with the latest mental health tips, news, and upcoming events.";
+  const buttonText = "Sign Up For Our Newsletter";
+
+  return (
+    <ContactSection
+      title={title}
+      description={description}
+      buttonText={buttonText}
+    />
+  );
+};
+
+export default ContactUs;
