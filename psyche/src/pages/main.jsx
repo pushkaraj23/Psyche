405cd83e767e0841a4b03bbf627bb0765d5664b0
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-10-27 12:02:02 +0530

    feat: add landing page

--- psyche/src/pages/main.jsx
+++ psyche/src/pages/main.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import Navbar from '../components/navbar'
-import Services from './services'
-import ContactUs from '../components/ContactUs'
-import PhotoGallery from './photoGallery'
+import React from "react";
+import Navbar from "../components/navbar";
+import Services from "./services";
+import ContactUs from "../components/ContactUs";
+import PhotoGallery from "./photoGallery";
+import Landing from "./landing";
 
 const main = () => {
   return (
     <div>
       <Navbar />
+      <Landing />
       <Services />
       <ContactUs />
       <PhotoGallery />
-      
     </div>
-  )
-}
+  );
+};
 
-export default main
+export default main;
 // You need to import all the pages here.
