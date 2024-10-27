4e10a1a680042ce49faa20bf6a64faff99ee6d84
ShashankS2213 <shashanksonawane2213@gmail.com>
2024-10-27 21:35:12 +0530

    footer complete

--- psyche/src/pages/footer.jsx
+++ psyche/src/pages/footer.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 // import Logo from "../assets/logo2.svg";
 import image from "../assets/image.png";
-import { EnvelopeIcon } from '@heroicons/react';
+import EnvelopeIcon from '@heroicons/react/24/outline/EnvelopeIcon';
+import PhoneIcon from '@heroicons/react/24/outline/PhoneIcon';
+import MapPinIcon from '@heroicons/react/24/outline/MapPinIcon';
 
 const Footer = () => {
     return (
@@ -38,18 +40,15 @@ const Footer = () => {
             </div>
 
             {/* Contact Information Section */}
-            <div className="relative z-10 flex-2 flex flex-col items-start gap-[1vw] text-[#ffffff] text-[1vw] pb-[7.5vw]">
+            <div className="relative flex flex-col items-start gap-[1vw] text-[#ffffff] text-[1vw] pb-[6.8vw]">
                 <h2 className="font-bold text-white text-[1.3vw] mb-[1vw] font-merriweather">Contact Information</h2>
-                <p class="flex items-center text-white">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                    </svg>Hadapsar, Pune - 412201</p>
-                <p class="flex items-center text-white">
-                    <svg class="w-6 h-6 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z" />
-                    </svg>+91 1234390822</p>
-                <p class="flex items-start text-white">
+                <p className="flex items-center text-white">
+                    <MapPinIcon className="w-6 h-6 mr-2" />Hadapsar, Pune - 412201</p>
+                <p className="flex items-center text-white">
+                    <PhoneIcon className="w-6 h-6 mr-2" />
+                    +1 234 567 890
+                </p>
+                <p className="flex items-center text-white">
                     <EnvelopeIcon className="w-6 h-6 mr-2" />
                     info@psyche.com
                 </p>
