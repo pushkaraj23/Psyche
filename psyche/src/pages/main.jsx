a72cb1c30582db63d61a319b73fcf9831b4ce44d
ShashankS2213 <shashanksonawane2213@gmail.com>
2024-10-27 18:07:43 +0530

    footer rendering

--- psyche/src/pages/main.jsx
+++ psyche/src/pages/main.jsx
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import Services from './services'
-import ContactUs from '../components/ContactUs'
-import PhotoGallery from './photoGallery'
-import Home from './footer'
-import LandingHeader from './footer'
-import Footer from './footer'
-import Home2 from './footer'
-=======
 import React from "react";
 import Navbar from "../components/navbar";
 import Services from "./services";
@@ -16,7 +6,6 @@ import Community from "../components/Community";
 import PhotoGallery from "./photoGallery";
 import Landing from "./landing";
 import FAQs from "./FAQs";
->>>>>>> bd091232286f58cab6961e5f0f2cb9831361d66d
 
 const main = () => {
   return (
@@ -25,14 +14,10 @@ const main = () => {
       <Landing />
       <Services />
       <PhotoGallery />
-<<<<<<< HEAD
-      <ContactUs />
-      <Footer />
-=======
       <Community />
       <FAQs />
       <ContactUs />
->>>>>>> bd091232286f58cab6961e5f0f2cb9831361d66d
+      <Footer />
     </div>
   );
 };
