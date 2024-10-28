6396afb06f9ac92ca0d69494011e524b5ae906bf
ShashankS2213 <shashanksonawane2213@gmail.com>
2024-10-28 18:05:03 +0530

    minor syntax changes

--- psyche/src/pages/footer.jsx
+++ psyche/src/pages/footer.jsx
@@ -13,14 +13,14 @@ const Footer = () => {
             ></div>
 
             {/* Title Section */}
-            <div className="relative z-10 flex-1 flex justify-center p-[10vw]">
-                <h1 className="text-white w-[17vw] text-[3.5vw] font-black leading-[4vw] font-merriweather">
+            <div className=" flex-1 justify-center m-[13vw] ">
+                <h1 className="text-white w-[17vw] text-[3.5vw] font-black leading-[4vw] font-merriweather p-[3vw]">
                     ONE MIND AT A TIME
                 </h1>
             </div>
 
             {/* Quick Links Section */}
-            <div className="relative z-10 flex-2 flex flex-col items-start gap-[1vw] text-[#FFFFFF] font-light text-[1vw] font-merriweather px-[5vw]">
+            <div className="flex flex-col items-start gap-[1vw] text-[#FFFFFF] font-light text-[1vw] font-merriweather px-[5vw]">
                 <h2 className="font-bold text-white text-[1.3vw] mb-[1vw]">Quick Links</h2>
                 <a href="#home" className="underline">Home</a>
                 <a href="#services" className="underline">Services</a>
@@ -31,7 +31,7 @@ const Footer = () => {
             </div>
 
             {/* Contact Information Section */}
-            <div className="relative flex flex-col items-start gap-[1vw] text-[#ffffff] text-[1vw] pb-[6.8vw]">
+            <div className="flex-col items-start gap-[1vw] text-[#ffffff] text-[1vw] pb-[9.5vw]">
                 <h2 className="font-bold text-white text-[1.3vw] mb-[1vw] font-merriweather">Contact Information</h2>
                 <p className="flex items-center text-white">
                     <MapPinIcon className="w-6 h-6 mr-2" />Hadapsar, Pune - 412201</p>
@@ -44,7 +44,7 @@ const Footer = () => {
                     info@psyche.com
                 </p>
 
-                
+
             </div>
         </div>
 
