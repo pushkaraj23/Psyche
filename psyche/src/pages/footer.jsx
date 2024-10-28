f9fc229ba0e12ff0dab8e2db1979f063bd87db1e
ShashankS2213 <shashanksonawane2213@gmail.com>
2024-10-28 19:34:10 +0530

    temp logo added to the footer

--- psyche/src/pages/footer.jsx
+++ psyche/src/pages/footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import image from "../assets/image.png";
+import Logo from "../assets/Logo.png";
 import { EnvelopeIcon, PhoneIcon, MapPinIcon } from '@heroicons/react/24/outline';
 
 const Footer = () => {
     return (
-        <div className="relative w-full h-[30vw] flex items-center justify-between px-[10vw] py-[5vw]">
+        <div className="relative w-full h-[30vw] flex items-center justify-between px-[8vw] py-[5vw]">
             <div className="absolute inset-0 bg-cover blur-[0.3vw] before:absolute before:inset-0 before:bg-[#717253] before:opacity-65"
                 style={{
                     backgroundImage: `url(${image})`,
@@ -12,15 +13,17 @@ const Footer = () => {
                 }}
             ></div>
 
-            {/* Title Section */}
-            <div className=" flex-1 justify-center m-[13vw] ">
-                <h1 className="text-white w-[17vw] text-[3.5vw] font-black leading-[4vw] font-merriweather p-[3vw]">
+            {/* Title and Logo Section */}
+            <div className="flex justify-center ">
+                
+                <img src={Logo} alt="logo" className="w-[14vw] m-[2vw]" />
+                <h1 className="text-white w-[17vw] text-[3.5vw] font-black leading-[4vw] my-[2vw] font-merriweather">
                     ONE MIND AT A TIME
                 </h1>
             </div>
 
             {/* Quick Links Section */}
-            <div className="flex flex-col items-start gap-[1vw] text-[#FFFFFF] font-light text-[1vw] font-merriweather px-[5vw]">
+            <div className="flex flex-col items-start gap-[1vw] text-[#FFFFFF] font-light text-[1vw] font-merriweather ml-[15vw]">
                 <h2 className="font-bold text-white text-[1.3vw] mb-[1vw]">Quick Links</h2>
                 <a href="#home" className="underline">Home</a>
                 <a href="#services" className="underline">Services</a>
@@ -31,7 +34,7 @@ const Footer = () => {
             </div>
 
             {/* Contact Information Section */}
-            <div className="flex-col items-start gap-[1vw] text-[#ffffff] text-[1vw] pb-[9.5vw]">
+            <div className="flex-col items-start text-[#ffffff] text-[1vw] mb-[9vw]">
                 <h2 className="font-bold text-white text-[1.3vw] mb-[1vw] font-merriweather">Contact Information</h2>
                 <p className="flex items-center text-white">
                     <MapPinIcon className="w-6 h-6 mr-2" />Hadapsar, Pune - 412201</p>
