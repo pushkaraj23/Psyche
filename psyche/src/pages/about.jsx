d95848d70e6a7adf70f890a4f0edc17fe79d936c
taketec <shailupandit293@gmail.com>
2024-11-06 12:08:53 +0530

    added manifesto

--- psyche/src/pages/about.jsx
+++ psyche/src/pages/about.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Navbar from "../components/navbar";
 import Footer from './footer';
 import { AboutLanding, OurStory, Reason } from '../components/about/aboutLanding'
-
+import Manifesto from "../components/about/manifesto";
 const about = () => {
   return (
     <div>
       <Navbar />
       <AboutLanding />
+      <Manifesto />
       <OurStory />
       <Reason />
       <Footer />
