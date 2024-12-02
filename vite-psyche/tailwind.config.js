2e7f232d668a7387d05c2eae431eaf3756518824
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-12-02 18:37:26 +0530

    add tailwind config

--- vite-psyche/tailwind.config.js
+++ vite-psyche/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        merriweather: ["Merriweather", "serif"],
+      },
+    },
   },
   plugins: [],
 };
