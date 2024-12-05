18731ef2b3dc82cb6c2b0b37ae95c9374c888d4b
pushkaraj23 <pushkaraj232002@gmail.com>
2024-12-06 00:56:50 +0530

    few changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./pages/main";
 import About from "./pages/about";
-import services from "./pages/Services";
-import contact from "./pages/contact";
+import Services from "./pages/Services";
+import Contact from "./pages/contact";
 
 function App() {
   return (
@@ -11,8 +11,8 @@ function App() {
       <Routes>
         <Route path="/" Component={Main} />
         <Route path="/about" Component={About} />
-        <Route path="/Services" Component={services} />
-        <Route path="/contact" Component={contact} />
+        <Route path="/Services" Component={Services} />
+        <Route path="/contact" Component={Contact} />
       </Routes>
     </Router>
   );
